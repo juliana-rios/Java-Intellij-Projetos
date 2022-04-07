bf1d5c34764862b3076b8e04b68abdb8f44e92c9
Juliana Rios <julianariosone@gmail.com>
2022-04-06 23:49:23 -0300

    update exercico metodo 2

---
+++ ExercicioMetodo2/src/Quadrilatero.java
@@ -0,0 +1,14 @@
+public class Quadrilatero {
+        public static void area(double lado){
+            System.out.println("Área do Quadrado: " + lado*lado);
+        }
+        public static void area(double lado1, double lado2){
+            System.out.println("Área do Triangulo: " + lado1*lado2);
+        }
+        public static void area(double baseMaior, double baseMenor, double altura){
+            System.out.println("Área trapézio: " + ((baseMaior+baseMenor)*altura) / 2 );
+        }
+    public static void area(float diagonal1, float diagonal2){
+        System.out.println("Área do Retangulo: " + (diagonal1*diagonal2) / 2);
+    }
+}
