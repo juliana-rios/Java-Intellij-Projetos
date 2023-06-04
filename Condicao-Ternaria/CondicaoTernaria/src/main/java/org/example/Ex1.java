f836cfa6aa6bae0e215f33e19acc8b9db7db7db9
Juliana Rios <julianariosone@gmail.com>
2023-06-04 20:05:10 -0300

    update novo exercicio

---
+++ Condicao-Ternaria/CondicaoTernaria/src/main/java/org/example/Ex1.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
